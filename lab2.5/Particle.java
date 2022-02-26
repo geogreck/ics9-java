2045c1e27aeadb91530e90461d610a46d617597f
Georgy Grechko <georgre4ko@ya.ru>
2022-02-26 19:00:42 +0300

    minor changes

--- lab2.5/Particle.java
+++ lab2.5/Particle.java
@@ -1,12 +1,24 @@
+import java.lang.Math;
+
 public class Particle {
     private static int n = 0;
-    private double mass, speed;
+    private double mass, speed, x, y, z;
     private static double energy = 0;
 
     public Particle(double inMass, double inSpeed){
         Particle.n += 1; 
         this.mass = inMass;
         this.speed = inSpeed;
+        Particle.energy += mass * speed * speed / 2;
+    }
+
+    public Particle(double inMass, double inX, double inY, double inZ){
+        Particle.n += 1;
+        this.mass = inMass;
+        this.x = inX;
+        this.y = inY;
+        this.z = inZ;
+        this.speed = Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
         Particle.energy += mass * speed * speed / 2;
     }
 
